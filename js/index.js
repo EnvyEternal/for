1fa275e681ec2166a6af9c2d17fb81c089d66421
Eternal <vladultimate@gmail.com>
2021-08-22 11:17:57 +0300

    A

---
+++ js/index.js
@@ -0,0 +1,29 @@
+/*let a = +prompt("1")
+let b = +prompt("2")
+function pow(a, b) {
+    let result = a;
+    for (let i = 1; i < b; i++) {
+      result *= a
+    }
+   
+    return result;
+
+  }
+  
+  alert(pow(a,b))*/
+ 
+/*function value(a){
+    let num = 1000;
+    a = num
+  while  (num > 50) {
+    console.log(num)  
+    num = num / 2;
+    
+  }
+  return num;
+}
+
+console.log(value())*/
+
+var x = 136;
+console.log(Math.round(x / 10) * 10);
